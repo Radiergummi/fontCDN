ba220362c302c0d83c0beb0d93b19e14fbe2ea64
Mo <Radiergummi@users.noreply.github.com>
2015-03-20 17:03:16 +0100

    Update uri.php

--- system/uri.php
+++ system/uri.php
@@ -1,22 +1,36 @@
 <?
 
 class Uri {
-  private uri;
+  /**
+   * holds the current uri
+   * 
+   * @var array
+   */
+  private $current = array();
 
   public function __construct() {
-    $this->uri = parse($_SERVER['REQUEST_URI']);
+    $this->current = parse($_SERVER['REQUEST_URI']);
   }
 
-  public function parse($uri) {
+  public function parse(string $uri) {
     if($uri = filter_var($uri, FILTER_SANITIZE_URL)) {
       // remove possible subfolder, trim the uri from slashes and explode the segments into an array.
       return explode('/', trim(str_replace(Config::get('app.relativepath'), '', $uri), '/'));
     }
   }
   
-  public function get($segment = null) {
+  /**
+   * get one (or all) segments of the current uri
+   * 
+   * @param string $segment (optional) segment to return.
+   * 
+   * @return string the segment of the uri
+   */
+  public function get(string $segment = null) {
     if (! empty($segment)) {
       
+    } else {
+      return $this->current;
     }
   }
 }
